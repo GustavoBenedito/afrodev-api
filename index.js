de5043f1999485fad8f8d4134a5f88b06fd5d01a
Gustavo Benedito <gustavobeneditodeveloper@gmail.com>
2021-03-31 19:33:35 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const express = require('express');
+const configExpress = require('./config/configExpress');
 const app = express();
 app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+
+app.get("/" ,(req, resp) => {
+    resp.send('servidor OK');
+});
---
+++ routes.js
@@ -0,0 +1,6 @@
+module.export = app => {
+    app.get('/', (req,resp) => {
+    resp.send('servidor OK');
+});
+app.post()
+};
