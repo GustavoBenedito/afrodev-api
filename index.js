3a788f74fc1345b56b56f99e0246bb6618c825b6
Gustavo Benedito <gustavobeneditodeveloper@gmail.com>
2021-03-31 20:11:26 -0300

    tabelas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const configExpress = require('./config/configExpress');
+const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/Tabelas')
+conexao.connect(error => {
+    if(error){
+        throw error;
+    }
+    Tabelas.init(conexao)
+})
 const app = express();
-app.listen(3000, () => console.log("servidor rodando na porta 3000"));
-
-app.get("/" ,(req, resp) => {
-    resp.send('servidor OK');
-});
+app.listen(3000, () => 
+console.log("servidor rodando na porta 3000"));
