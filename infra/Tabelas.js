bcd55c60457e394f320cb20d2c68d67a5cfe8730
Lucas Sousa <Lucas996oliveira@gmail.com>
2021-04-02 20:47:49 -0300

    Refatoração

--- infra/Tabelas.js
+++ infra/Tabelas.js
@@ -5,9 +5,11 @@ class Tabelas {
     };
 
     criarAgendamento() {
-        const sql = `CREATE TABLE IF NOT EXISTS agendamentos(id int NOT NULL AUTO_INCREMENT,nome_cliente varchar(50) NOT NULL, servico varchar(50) NOT NULL, status varchar(20) NOT NULL, data_servico date NOT NULL, data_agendamento date NOT NULL, PRIMARY KEY(id))`
+        const sql = `CREATE TABLE IF NOT EXISTS agendamentos (id int NOT NULL AUTO_INCREMENT,
+            nome_cliente varchar(50) NOT NULL, servico varchar(50) NOT NULL,
+            status varchar(20) NOT NULL, data_servico date NOT NULL, data_agendamento date NOT NULL, PRIMARY KEY(id))`;
 
-        this.conexao.query(sql, error => {
+        this.conexao.query(sql, (error) => {
             if (error) {
                 throw error
             }
--- models/Agendamento.js
+++ models/Agendamento.js
@@ -3,34 +3,49 @@ const moment = require('moment');
 
 class Agendamento{
     
-    inserir(agendamento){
-        const sql = 'INSERT INTO agendamentos SET ? ';
+    inserir(agendamento, resp){
+        const sql = "insert into agendamentos set ?";
 
-        const data_servico = moment(agendamento.data_servico).format('YYYY-MM-DD');
-        const data_agendamento = moment().format('YYYY-MM-DD');
-        const agendamentoComData = {...agendamento, data_agendamento, data_servico};
+    const data_servico = moment(agendamento.data_servico).format("YYYY-MM-DD");
+    const data_agendamento = moment().format("YYYY-MM-DD");
+    const agendamentoComData = {
+      ...agendamento,
+      data_agendamento,
+      data_servico,
+    };
 
-        const isDataValida = moment(agendamento.data_servico).isSameOrAfter(agendamento.data_agendamento);
-        const isNomeClienteValido = agendamento.nome_cliente.lenght >= 3;
+    const isValid = moment(agendamento.data_servico).isSameOrAfter(
+      agendamento.data_agendamento
+    );
 
-        const validacoes = [
-            {
-                nome: "data_servico",
-                valido: isDataValida,
-                mensagem: "Data do Agendamento deve ser igual ou superior a atual"
-            },
-            {
-                nome: "nome_cliente",
-                valido: isNomeClienteValido,
-                mensagem: "O nome do cliente deve ter pelo menos 3 caracteres"
-            }
-        ]
-        conexao.query(sql, agendamentoComData, (error,results)=>{
-            if(error){
-                throw error
-            }
-            console.log(results)
-        });
+    const nameIsValid = agendamento.nome_cliente.length >= 3;
+
+    const validacoes = [
+      {
+        name: "data_servico",
+        valid: isValid,
+        mensage: "Data do Agendamento deve ser igual ou superior a atual",
+      },
+
+      {
+        name: "nome_cliente",
+        valid: nameIsValid,
+        mensage: "O nome do cliente deve ter pelo menos 3 caracteres",
+      },
+    ];
+
+    const errors = validacoes.filter((campo) => !campo.valid);
+
+    if (errors.length > 0) {
+      return resp.status(400).json(errors);
+    }
+
+    con.query(sql, agendamentoComData, (error, results) => {
+      if(error) {
+        resp.status(401).json(error)
+      }
+       resp.status(201).json({ ...agendamentoComData, id: results.insertId });
+    });
     } 
 }
 
